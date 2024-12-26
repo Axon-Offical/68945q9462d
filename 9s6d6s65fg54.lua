0dcc2337c1756b5757c2ce00d13fcd6fbec02cee
Storm Softworks <106533596+StormSoftworks@users.noreply.github.com>
2024-12-26 18:05:56 +0200

    Create 9s6d6s65fg54.lua

---
+++ 9s6d6s65fg54.lua
@@ -0,0 +1,29 @@
+local Keys = {
+	"AbCdEfGhIjKlMnOpQrSt";
+	"XyZaBcDeFgHiJkLmNoPq";
+	"QrStUvWxYzAbCdEfGhIj";
+	"KlMnOpQrStUvWxYzAbCd";
+	"EfGhIjKlMnOpQrStUvWx";
+	"YzAbCdEfGhIjKlMnOpQr";
+	"StUvWxYzAbCdEfGhIjKl";
+	"MnOpQrStUvWxYzAbCdEf";
+	"GhIjKlMnOpQrStUvWxYz";
+	"AbCdEfGhIjKlMnOpQrSt";
+	"XyZaBcDeFgHiJkLmNoPq";
+	"QrStUvWxYzAbCdEfGhIj";
+	"KlMnOpQrStUvWxYzAbCd";
+	"EfGhIjKlMnOpQrStUvWx";
+	"YzAbCdEfGhIjKlMnOpQr";
+	"StUvWxYzAbCdEfGhIjKl";
+	"MnOpQrStUvWxYzAbCdEf";
+	"GhIjKlMnOpQrStUvWxYz";
+	"AbCdEfGhIjKlMnOpQrSt";
+	"XyZaBcDeFgHiJkLmNoPq";
+	"QrStUvWxYzAbCdEfGhIj";
+	"KlMnOpQrStUvWxYzAbCd";
+	"EfGhIjKlMnOpQrStUvWx";
+	"YzAbCdEfGhIjKlMnOpQr";
+	"StUvWxYzAbCdEfGhIjKl";
+}
+
+return Keys
